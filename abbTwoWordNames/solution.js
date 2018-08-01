21cd7a4113f4a2a1f3d79fcd5b62a56588aa24b6
alexlhanson <alexlhanson@gmail.com>
2018-08-01 10:15:44 -0700

    added use strict

--- abbTwoWordNames/solution.js
+++ abbTwoWordNames/solution.js
@@ -1,3 +1,4 @@
+'use strict';
 function abbrevName(name){
   //splits name into array, grabs capital first char of each and joins them with dot
     return name.split(" ").map(x => x[0].toUpperCase()).join('.');
